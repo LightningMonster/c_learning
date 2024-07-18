235873814610b0084330366bffc150fe9743ef06
LightningMonster <lightningmonster.1548@gmail.com>
2024-07-18 21:30:48 +0530

    Escape sequence

---
+++ Day 1/escape_sequences.c
@@ -0,0 +1,11 @@
+/* \t	Creates a horizontal tab	
+\\	Inserts a backslash character (\)	
+\"	Inserts a double quote character */
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  printf("\"I am learning C.\" \t \\ ");
+  return 0;
+}
