c93245f08714a8755ba7d5db00842ca020bfb17a
LightningMonster <lightningmonster.1548@gmail.com>
2024-07-18 21:40:45 +0530

    Variables

--- Day 1/variables.c
+++ Day 1/variables.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  int a = 5;
-  printf("%d",a);
-  return 0;
+
+// Create variables
+    int myNum = 15;            // Integer (whole number)
+    float myFloatNum = 5.99;   // Floating point number
+    char myLetter = 'D';       // Character
+
+// Print variables
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+    return 0;
 }
