c2143699e5192b9e05aee985f76b4d1383a9bd57
Yash Dhadve <lightningmonster.1548@gmail.com>
2024-10-12 22:46:49 +0530

    test1

--- OS_2/Q25.c
+++ OS_2/Q25.c
@@ -1,5 +1,5 @@
 /* 21.2
-Write a C program to simulate Preemptive Priority scheduling. The arrival
+Q25)Write a C program to simulate Preemptive Priority scheduling. The arrival
 time and first CPU-burst and priority for different n number of
 processes should be input to the algorithm. Assume the fixed IO
 waiting time (2 units). The next CPU-burst should be generated randomly. The
